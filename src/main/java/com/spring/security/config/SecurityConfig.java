72a82b61540b6d6036ea8d3378de6e676af8ab33
prasaddharani <dharaniprasad1999@gmail.com>
2025-07-19 12:21:35 +0530

    Multiple users access

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,20 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService () {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                            .username("Dharani")
+                            .password("Dharani")
+                            .roles("USER")
+                            .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("Prasanth")
+                .password("Prasanth")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
