653c4bed32e9c82f3332448b23a112bc9b7ed002
prasaddharani <dharaniprasad1999@gmail.com>
2025-07-19 11:30:03 +0530

    Setting up username and password

--- src/main/java/com/spring/security/controller/HelloController.java
+++ src/main/java/com/spring/security/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.spring.security.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    private ResponseEntity<String> getHello() {
-        return new ResponseEntity<>("hello", HttpStatusCode.valueOf(200));
+    private ResponseEntity<String> getHello(HttpServletRequest request) {
+        return new ResponseEntity<>("Hello " + request.getSession().getId(),
+                HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping("/about")
+    private ResponseEntity<String> getAbout(HttpServletRequest request) {
+        return new ResponseEntity<>("About Dharani " + request.getSession().getId(),
+                HttpStatusCode.valueOf(200));
     }
 }
